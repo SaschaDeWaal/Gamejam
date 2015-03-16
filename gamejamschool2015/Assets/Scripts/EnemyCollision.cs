f641463388992bb8468fdb06f58836060ef4a198
SaschaDeWaal <info@dreamfoxgames.com>
2015-03-16 13:34:12 +0100

    enemies worden weg geduwt bij aanraking

---
+++ gamejamschool2015/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Player"){
+			Rigidbody body = GetComponent<Rigidbody>();
+			body.AddForce((transform.position-col.transform.position+new Vector3(0,10,0))*200);
+		}
+	}
+}
